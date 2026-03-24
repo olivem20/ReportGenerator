8c70fac45f0c9b12b051a29233a03ac7cc975e7f
Olive Maunupau <olivemaunupau@Olives-MacBook-Air.local>
2026-03-24 15:53:43 -0400

    font working???

--- tabs/serve_profile.py
+++ tabs/serve_profile.py
@@ -205,7 +205,7 @@ def render_serve_profile(df, player):
         draw = ImageDraw.Draw(img)
 
         # If you have a .ttf font file, use it; otherwise PIL default
-        font = get_font(120)
+        font = get_font(35)
         #font = ImageFont.load_default()
 
         draw.text((177, 95), "Key", fill=(0,0,0,255), font=font) #USAGE %
